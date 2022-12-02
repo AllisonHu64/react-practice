6b7eecdc7716e41f8378639483dd7cbac0337267
Allison Hu <pan.hu46@gmail.com>
2022-12-02 04:45:35 -0500

    Initialize UserCard component

---
+++ src/components/cards/UserCard.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+
+function UserCard(props) {
+    const { user } = props;
+    return (
+    <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+            <Typography noWrap gutterBottom variant="h5" component="div">
+                Name: {user.name}
+            </Typography>
+            <Typography noWrap variant="body2" color="text.secondary">
+                Age: {user.age}
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Button size="small" startIcon={<EditIcon />}>Edit</Button>
+            <Button size="small" startIcon={<DeleteIcon />} color="error">Delete</Button>
+        </CardActions>
+    </Card>
+    )
+}
+
+export default UserCard
