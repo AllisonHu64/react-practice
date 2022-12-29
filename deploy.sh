4509b6be9c261b487fd62c0820252c7b9157d7d9
Allison Hu <pan.hu46@gmail.com>
2022-12-29 16:04:12 -0500

    Modify Dockerfile and modify deploy.sh prod commands
    - replace `git clone url` to `copy ./build` in Dockerfile
    - add yarn build in deploy.sh when env=prod

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 if [ $env == "dev" ]; then
-    # docker stop react-practice
-    # docker rm react-practice
-    docker build -t custom-nginx .
-    docker run --name react-practice -d -p 8080:80 custom-nginx
+    yarn build
+    docker build -t front-end:1.3 . 
+    # TODO fix this, error with env passing
+    docker stop react-practice
+    docker rm react-practice
+    docker run --name react-practice -d -p 8080:80 \
+    -e REACT_APP_BACKEND_DOMAIN=$REACT_APP_BACKEND_DOMAIN \
+    -e REACT_APP_BACKEND_PORT=$REACT_APP_BACKEND_PORT \
+    front-end:1.3
 elif [ $env == 'prod' ]; then
     # TODO
+    yarn build
+    docker build -t front-end:1.3 . 
 fi
