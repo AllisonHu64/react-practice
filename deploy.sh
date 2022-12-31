ee9b19a8e69d25d75e0b27388a65b14a8c2f8b53
Allison Hu <pan.hu46@gmail.com>
2022-12-31 00:06:19 -0500

    Fix bug in passing env

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,8 @@
-if [ $env == "dev" ]; then
-    yarn build
-    docker build -t front-end:1.3 . 
-    # TODO fix this, error with env passing
+if [[ $env == "dev" ]]; then
+    docker build -t front-end:1.3 --build-arg ENV_FILE=.env.development . 
     docker stop react-practice
     docker rm react-practice
-    docker run --name react-practice -d -p 8080:80 \
-    -e REACT_APP_BACKEND_DOMAIN=$REACT_APP_BACKEND_DOMAIN \
-    -e REACT_APP_BACKEND_PORT=$REACT_APP_BACKEND_PORT \
-    front-end:1.3
-elif [ $env == 'prod' ]; then
-    # TODO
-    yarn build
-    docker build -t front-end:1.3 . 
+    docker run --name react-practice -d -p 8080:80 front-end:1.3
+elif [[ $env == 'prod' ]]; then
+    docker build -t front-end:1.3 --build-arg ENV_FILE=.env.production . 
 fi
