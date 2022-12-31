9c26cddfb56a4e8a5fb549cee617e44b88bb1c26
Allison Hu <pan.hu46@gmail.com>
2022-12-31 02:01:26 -0500

    Add -v flags in ./deploy.sh to specify docker image version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,24 @@
+VERSION="lastest"
+while getopts ":v:" options; do
+    case "${options}" in
+    v)
+        VERSION=${OPTARG}
+        ;;
+    :)
+        echo "Error: -${OPTARG} requires an argument."
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
+  esac
+done
+
 if [[ $env == "dev" ]]; then
-    docker build -t front-end:1.3 --build-arg ENV_FILE=.env.development . 
+    docker build -t front-end:${VERSION} --build-arg ENV_FILE=.env.development . 
     docker stop react-practice
     docker rm react-practice
-    docker run --name react-practice -d -p 8080:80 front-end:1.3
+    docker run --name react-practice -d -p 8080:80 front-end:${VERSION}
 elif [[ $env == 'prod' ]]; then
-    docker build -t front-end:1.3 --build-arg ENV_FILE=.env.production . 
+    docker build -t front-end:${VERSION} --build-arg ENV_FILE=.env.production . 
 fi
